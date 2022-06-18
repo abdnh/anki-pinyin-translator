2a64cd6f8caf8d6cbb0127b1d62d926aea47cd16
Abdo <abd.nh25@gmail.com>
2022-06-18 19:53:04 +0300

    Specify key for getFile

--- src/__init__.py
+++ src/__init__.py
@@ -18,7 +18,12 @@ from .notetype import PinyinNotetype
 
 def on_import() -> None:
     files = getFile(
-        mw, title="Choose CSV files to import", cb=None, filter="(*.csv *.txt)", multi=True
+        mw,
+        title="Choose CSV files to import",
+        cb=None,
+        key="pinyin_translator",
+        filter="(*.csv *.txt)",
+        multi=True,
     )
     if not files:
         return
